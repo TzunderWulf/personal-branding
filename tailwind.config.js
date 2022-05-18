d531c3e29353e61d3887481d4f0cde6f76dba1ed
TzunderWulf <svandenbos@tzunder.nl>
2022-05-18 23:30:16 +0200

    Added a projects section with stock projects.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
@@ -6,6 +8,10 @@ module.exports = {
         darkBlue: '#14213d',
         yellowOrange: '#FCA311',
         softWhite: '#E5E5E5',
+      },
+      fontFamily: {
+        heading: ['Koulen', ...defaultTheme.fontFamily.serif],
+        standard: ['Ubuntu', ...defaultTheme.fontFamily.sans]
       },
     },
   },
